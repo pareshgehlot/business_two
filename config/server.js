87117aa2b930c1feb7740a9658798c18fc52cfd1
Paresh Gehlot <gehlot.paresh@gmail.com>
2021-06-12 19:47:36 -0400

    uploaded server.js config at right place
    uploaded server.js config at right place

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 const server_object = {
-  server_ip: process.env.SERVER_IP || "127.0.0.1",
+  server_ip: process.env.SERVER_IP || "0.0.0.0",
   server_port: process.env.PORT || 5000
 };
 module.exports = server_object;
